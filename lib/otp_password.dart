3239b23c262113816d5a7f75d549db10f844a6f5
abiodun emma <abiodunemma769@gmail.com>
2026-02-02 14:07:21 +0100

    otp

---
+++ lib/otp_password.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class OTPScreen extends StatelessWidget {
+  const OTPScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    )
+
+  }
+}
