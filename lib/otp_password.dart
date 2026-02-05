0773eb43189b37d4a9e66a8406af42350cebe3b6
abiodun emma <abiodunemma769@gmail.com>
2026-02-05 12:32:43 +0100

    load

--- lib/otp_password.dart
+++ lib/otp_password.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+
+
 
 class OTPScreen extends StatelessWidget {
   const OTPScreen({super.key});
@@ -24,8 +27,138 @@ class OTPScreen extends StatelessWidget {
                       },
                     ),
                   ),
+                  const Text(
+                    "Login to Foodly",
+                    style: TextStyle(
+                      fontSize: 18,
+                     
+                    )
+
+                  )
                 ],
               ),
+              const SizedBox(height: 20),
+
+              const Text(
+                "Verify phone number",
+                style: TextStyle(
+                  fontSize: 22,
+                   fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+
+              const SizedBox(height: 10),
+
+              Center(
+  child: const Text(
+    "Enter the 4-Digit code sent to you at +610489632578",
+    textAlign: TextAlign.center,
+    style: TextStyle(
+      fontSize: 14,
+      color: Colors.black54,
+    ),
+  ),
+),
+
+
+    PinCodeTextField(
+      appContext: context,
+      length: 4,
+      keyboardType: TextInputType.number,
+      autoFocus: true,
+      animationType: AnimationType.fade,
+
+      pinTheme: PinTheme(
+        shape: PinCodeFieldShape.underline, 
+        fieldHeight: 50,
+        fieldWidth: 40,
+        activeColor: Colors.black,
+        inactiveColor: Colors.grey,
+        selectedColor: Colors.blue,
+      ),
+
+      onChanged: (value) {},
+
+      onCompleted: (value) {
+        print("OTP Entered: $value");
+      },
+    ),
+                    const SizedBox(height: 30),
+
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {                     
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xffEEA734),
+                          padding: const EdgeInsets.symmetric(vertical: 16),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(14),
+                          ),
+                        ),
+                        child: const Text(
+                          "Continue",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                          )
+                        
+                        ),
+                      )
+                    ),
+
+                    const SizedBox(height: 15),
+
+                    Center(
+  child: RichText(
+    textAlign: TextAlign.center,
+    text: const TextSpan(
+      style: TextStyle(
+        fontSize: 14,
+        color: Colors.black54,
+      ),
+      children: [
+        TextSpan(
+          text: "Didn’t receive code?",
+        ),
+        TextSpan(
+          text: "Resend Again.",
+          style: TextStyle(
+            color: Color(0xffEEA734),
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    ),
+  ),
+),
+
+                    const SizedBox(height: 25),
+
+                                     Center(
+  child: const Text(
+    "By Signing up you agree to our Terms ",
+    textAlign: TextAlign.center,
+    style: TextStyle(
+      fontSize: 14,
+      color: Colors.black54,
+    ),
+  ),
+),
+                    
+                      Center(
+  child: const Text(
+    "Conditions & Privacy Policy.",
+    textAlign: TextAlign.center,
+    style: TextStyle(
+      fontSize: 14,
+      color: Colors.black54,
+    ),
+  ),
+),
+
             ],
           ),
         ),
