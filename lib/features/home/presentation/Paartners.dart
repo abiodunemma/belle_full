f5b36db5dca8350b29e5b89635566a9a66e23f3b
abiodun emma <abiodunemma769@gmail.com>
2026-02-16 10:20:27 +0100

    partners_page

--- lib/features/home/presentation/Paartners.dart
+++ lib/features/home/presentation/Paartners.dart
@@ -8,7 +8,39 @@ class PartnersScreeen extends StatefulWidget {
 }
 
 class _PartnersScreeenState extends State<PartnersScreeen> {
-
-
-
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 56,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: const Icon(Icons.arrow_back_ios)
+                      ),
+                    ),
+                    const Text(
+                      "Featured Partners"
+                      style: TextStyle(),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
