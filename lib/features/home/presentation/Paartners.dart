96aabd73035193c14e4309db186c00df6eb73d86
abiodun emma <abiodunemma769@gmail.com>
2026-02-16 10:06:35 +0100

    partners_page

---
+++ lib/features/home/presentation/Paartners.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PartnersScreeen extends StatefulWidget {
+  const PartnersScreeen({super.key});
+
+  @override
+  State<PartnersScreeen> createState() => _PartnersScreeenState();
+}
+
+class _PartnersScreeenState extends State<PartnersScreeen> {
+
+
+
+}
