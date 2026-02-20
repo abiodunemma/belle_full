2a6fff462ec3c799389762842f71f2e7342d1eb8
abiodun emma <abiodunemma769@gmail.com>
2026-02-20 11:03:10 +0100

    part_page

---
+++ lib/kitchen/kitchen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class kitchen extends StatefulWidget {
+  const kitchen({super.key});
+
+  @override
+  State<kitchen> createState() => _kitchenState();
+}
+
+class _kitchenState extends State<kitchen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
